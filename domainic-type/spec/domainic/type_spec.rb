e34a02eda0e9f1f023c4fecb17cc7b2aaeb8dade
Aaron Allen <hello@aaronmallen.me>
2024-10-10 14:01:29 -0500

    Add `Domainic::Type::UNSPECIFIED`
    Changelog:
      - added `Domainic::Type::UNSPECIFIED`

--- domainic-type/spec/domainic/type_spec.rb
+++ domainic-type/spec/domainic/type_spec.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'domainic/type/constants'
 
 RSpec.describe Domainic::Type do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '::UNSPECIFIED' do
+    subject(:unspecified) { described_class::UNSPECIFIED }
+
+    it { is_expected.to be_an_instance_of(Object).and(be_frozen) }
+  end
 end
