3adfe9009d37dfff75d918f37722fb2a01734f9c
Aaron Allen <hello@aaronmallen.me>
2024-10-10 14:19:02 -0500

    Add `Domainic::Type::Constraint::BaseConstraint`
    Changelog:
      - added `Domainic::Type::Constraint::BaseConstraint`
    
    see #32

---
+++ domainic-type/lib/domainic/type/constraint/base_constraint.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Domainic
+  module Type
+    module Constraint
+      # The base class for all constraints.
+      #
+      # @since 0.1.0
+      class BaseConstraint # rubocop:disable Lint/EmptyClass
+      end
+    end
+  end
+end
