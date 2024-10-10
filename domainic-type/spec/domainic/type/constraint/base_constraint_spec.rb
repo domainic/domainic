eb15343628132f34beb9bae158aaf3d786d23440
Aaron Allen <hello@aaronmallen.me>
2024-10-10 18:03:04 -0500

    Implement `Domainic::Type::Constraint::BaseConstraint`
    Changelog:
      - added `Domainic::Type::Constraint::BaseConstraint.parameters`
      - added `Domainic::Type::Constraint::BaseConstraint#description`
      - added `Domainic::Type::Constraint::BaseConstraint#name`
      - added `Domainic::Type::Constraint::BaseConstraint#parameters`
      - added `Domainic::Type::Constraint::BaseConstraint#validate`
      - changed `Domainic::Type::Constraint::BaseConstraint` now takes a
        `Domainic::Type::BaseType` instance, name, description, and
        parameter options as arguments.
    
    resolves #32

--- domainic-type/spec/domainic/type/constraint/base_constraint_spec.rb
+++ domainic-type/spec/domainic/type/constraint/base_constraint_spec.rb
@@ -1,8 +1,58 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'domainic/type/base_type'
 require 'domainic/type/constraint/base_constraint'
 
 RSpec.describe Domainic::Type::Constraint::BaseConstraint do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:type) { instance_double(Domainic::Type::BaseType) }
+
+  describe '.parameters' do
+    subject(:parameters) { described_class.parameters }
+
+    it { is_expected.to be_an_instance_of(Domainic::Type::Constraint::ParameterSet) }
+  end
+
+  describe '#initialize' do
+    subject(:new_instance) { constraint_class.new(type, **options) }
+
+    let(:constraint_class) { described_class }
+    let(:options) { {} }
+
+    it 'is expected to initialize with the base type' do
+      expect(new_instance.instance_variable_get(:@base)).to eq(type)
+    end
+
+    it 'is expected to initialize with parameters' do
+      expect(new_instance.parameters).to be_an_instance_of(Domainic::Type::Constraint::ParameterSet)
+    end
+
+    context 'when name and description is provided' do
+      let(:options) { { name: 'test', description: 'A test constraint' } }
+
+      it { is_expected.to have_attributes(name: options[:name].to_sym, description: options[:description]) }
+    end
+
+    context 'when name is not provided' do
+      it { is_expected.to have_attributes(name: :base) }
+    end
+
+    context 'when parameters are provided' do
+      let(:constraint_class) do
+        Class.new(described_class) do
+          def self.name
+            'TestConstraint'
+          end
+
+          parameters.add(name: :test)
+        end
+      end
+
+      let(:options) { { test: 'test' } }
+
+      it 'is expected to set the parameter value' do
+        expect(new_instance.parameters.test.value).to eq(options[:test])
+      end
+    end
+  end
 end
