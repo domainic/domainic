2cd55224b081e69488f6314d055e0d1b161d6b10
Aaron Allen <hello@aaronmallen.me>
2024-10-15 01:12:45 -0500

    Add `Domainic::Type::BaseType.constrain`
    Changelog:
      - added `Domainic::Type::BaseType.constrain`
    
    see #70

--- domainic-type/lib/domainic/type/base_type.rb
+++ domainic-type/lib/domainic/type/base_type.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'constraint/provisioning/constraint_set'
+require_relative 'dsl/constraint_builder'
 
 module Domainic
   module Type
@@ -16,11 +17,39 @@ module Domainic
       attr_reader :constraints
 
       class << self
+        # Provision a constraint for the type.
+        #
+        # @param accessor_name [String, Symbol] the name of the accessor.
+        # @yield [DSL::ConstraintBuilder] the block to define constraints.
+        # @return [void]
+        def constrain(accessor_name, &)
+          constraint_builder.define(accessor_name, &).build
+        end
+
         # The constraints for the type.
         #
         # @return [Constraint::Provisioning::ConstraintSet]
         def constraints
           @constraints ||= Constraint::Provisioning::ConstraintSet.new(self)
+        end
+
+        private
+
+        # The constraint builder for the type.
+        #
+        # @return [DSL::ConstraintBuilder]
+        def constraint_builder
+          @constraint_builder ||= DSL::ConstraintBuilder.new(self)
+        end
+
+        # Ensure constraints are properly inherited.
+        #
+        # @param subclass [Class<BaseType>] the subclass inheriting from BaseType.
+        # @return [void]
+        def inherited(subclass)
+          super
+          subclass.instance_variable_set(:@constraint_builder, constraint_builder.dup_with_base(subclass))
+          subclass.send(:constraint_builder).build
         end
       end
 
