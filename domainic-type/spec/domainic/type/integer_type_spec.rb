b4937a6a82911438d804bb65ca50b65d5dd8c705
Aaron Allen <hello@aaronmallen.me>
2024-10-18 15:44:44 -0500

    Add `Domainic::Type::IntegerType`
    Changelog:
      - added `Domainic::Type::IntegerType`
    
    see #65

---
+++ domainic-type/spec/domainic/type/integer_type_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/integer_type'
+
+RSpec.describe Domainic::Type::IntegerType do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
