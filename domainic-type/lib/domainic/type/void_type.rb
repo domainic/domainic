73640005ee37256cade2b30ca9940c622c147c2a
Aaron Allen <hello@aaronmallen.me>
2024-10-18 13:53:07 -0500

    Add `Domainic::Type::VoidType`
    Changelog:
      - added `Domainic::Type::VoidType`
    
    see #69

---
+++ domainic-type/lib/domainic/type/void_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'base_type'
+
+module Domainic
+  module Type
+    # @since 0.1.0
+    class VoidType < BaseType
+    end
+  end
+end
---
+++ domainic-type/spec/domainic/type/void_type_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/void_type'
+
+RSpec.describe Domainic::Type::VoidType do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
