79a70006b74981e28c6c2be48cb5e5f25d66b18c
Aaron Allen <hello@aaronmallen.me>
2024-10-18 14:51:31 -0500

    Add `Domainic::Type::StringType`
    Changelog:
      - added `Domainic::Type::StringType`
    
    see #67

---
+++ domainic-type/spec/domainic/type/string_type_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/string_type'
+
+RSpec.describe Domainic::Type::StringType do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
