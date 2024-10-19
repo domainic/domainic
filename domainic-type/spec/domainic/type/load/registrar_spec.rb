3d045dbb0c0f15d75b56de1ebebab52848a45113
Aaron Allen <hello@aaronmallen.me>
2024-10-19 13:23:17 -0500

    Add `Domainic::Type::Load::Registrar`
    Changelog:
      - added `Domainic::Type::Load::Registrar`
    
    see #73

---
+++ domainic-type/spec/domainic/type/load/registrar_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/load/registrar'
+
+RSpec.describe Domainic::Type::Load::Registrar do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
