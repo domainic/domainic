4ad8253f69e34ca11d30406c35ae7187e34711bf
Aaron Allen <hello@aaronmallen.me>
2024-10-20 19:00:30 -0500

    Add `Domainic::Type.config`
    Changelog:
      - added `Domainic::Type.config`
      - added `Domainic::Type.configure`
    
    see #75

--- domainic-type/lib/domainic/type.rb
+++ domainic-type/lib/domainic/type.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'type/configuration'
+
 module Domainic
   # Stupidly granular type validation for Ruby.
   #
   # @since 0.1.0
   module Type
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
+
+    def self.configure
+      yield configuration if block_given?
+    end
   end
 end
