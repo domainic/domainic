f50b753e2d060493e676b4290fe1507e7eae873f
Aaron Allen <hello@aaronmallen.me>
2024-12-02 14:22:56 -0600

    Add `Domainic::Attributer::Builder`

---
+++ domainic-attributer/lib/domainic/attributer/builder.rb
@@ -0,0 +1,232 @@
+# frozen_string_literal: true
+
+require 'domainic/attributer/attribute'
+require 'domainic/attributer/attribute_definition'
+require 'domainic/attributer/method_injector'
+
+module Domainic
+  module Attributer
+    # Builder provides a fluent interface for defining Attributes.
+    # It manages the lifecycle of attribute creation, from initial definition through
+    # configuration to final instantiation and method injection.
+    #
+    # The Builder accumulates attribute definitions and configurations before
+    # building them all at once or individually. Each attribute can be customized
+    # with coercers, validators, callbacks, and default values.
+    #
+    # @since 0.1.0
+    class Builder
+      # @rbs @base: untyped
+      # @rbs @current_definition: Hash[Symbol, untyped]?
+      # @rbs @data: Hash[Symbol, untyped]
+      # @rbs @method_injector: MethodInjector
+
+      # Creates a new Builder for the given base object.
+      #
+      # @param base [Class, Object] The class or instance to build attributes for
+      # @return [void]
+      # @rbs (untyped base) -> void
+      def initialize(base)
+        @base = base
+        @data = {}
+        @method_injector = MethodInjector.new(@base)
+      end
+
+      # Builds all pending attribute definitions or just the current one.
+      #
+      # @return [self] the Builder instance
+      # @rbs () -> self
+      def build!
+        @current_definition.nil? ? build_all_definitions : build_definition(current_definition)
+        @current_definition = nil
+        self
+      end
+
+      # Adds a coercer to transform attribute values.
+      #
+      # @param proc_symbol_or_true [Proc, Symbol, true, nil] The coercer to add
+      # @yield An optional block to use as the coercer
+      # @return [self] the Builder instance
+      # @rbs (
+      #   ^(untyped value) -> untyped | Symbol | true proc_symbol_or_true
+      #   ) ?{ (?) -> void } -> self
+      def coerce_with(proc_symbol_or_true = nil, &block)
+        ensure_current_definition!
+        current_definition[:attribute_options][:coercers] << (proc_symbol_or_true || block)
+        self
+      end
+      alias coerce coerce_with
+
+      # Sets a default value or generator for the attribute.
+      #
+      # @param value_or_proc [Object, Proc, nil] The default value or a proc to generate it
+      # @yield An optional block to generate the default value
+      # @return [self] the Builder instance
+      # @rbs (
+      #   ^() -> untyped | untyped value_or_proc,
+      #   ) ?{ (?) -> void } -> self
+      def default(value_or_proc = nil, &block)
+        ensure_current_definition!
+        current_definition[:attribute_options][:default] = (value_or_proc || block)
+        self
+      end
+
+      # Begins the definition of a new attribute.
+      #
+      # @param attribute_name [String, Symbol] The name of the attribute
+      # @param type_validator [Class, Module, Proc, nil] Optional type constraint
+      # @param options [Hash] Configuration options for the attribute
+      # @yield [Builder] An optional block for additional configuration
+      # @return [self] the Builder instance
+      # @rbs (
+      #   String | Symbol attribute_name,
+      #   Class | Module | ^(untyped value) -> bool type_validator,
+      #   **untyped options,
+      #   ) ?{ (?) [self: Builder] -> void } -> self
+      def define(attribute_name, type_validator = nil, **options, &block)
+        build_current_definition(attribute_name, type_validator, **options)
+        instance_exec(&block) if block
+        self
+      end
+
+      # Sets the description for the current attribute.
+      #
+      # @param text [String] Documentation for the attribute
+      # @return [self] the Builder instance
+      # @rbs (String text) -> self
+      def description(text)
+        ensure_current_definition!
+        current_definition[:attribute_options][:description] = text
+        self
+      end
+      alias desc description
+
+      # Creates a duplicate of this Builder with a new base object.
+      #
+      # @param new_base [Class, Object] The new base object
+      # @return [Builder] A new Builder instance
+      # @rbs (untyped new_base) -> instance
+      def dup_with_base(new_base)
+        dup.tap do |duped|
+          duped.instance_variable_set(:@base, new_base)
+          duped.instance_variable_set(:@method_injector, @method_injector.dup_with_base(new_base))
+        end
+      end
+
+      # Adds a callback to be executed when the attribute's value changes.
+      #
+      # @param proc [Proc, nil] The callback to execute
+      # @yield An optional block to use as the callback
+      # @return [self] the Builder instance
+      # @rbs (^(untyped value) -> void proc) ?{ (?) -> void } -> self
+      def on_change(proc = nil, &block)
+        ensure_current_definition!
+        current_definition[:attribute_options][:callbacks] << (proc || block)
+        self
+      end
+
+      # Makes the current attribute required (non-nil).
+      #
+      # @return [self] the Builder instance
+      # @rbs () -> self
+      def required
+        ensure_current_definition!
+        current_definition[:attribute_options][:required] = true
+        self
+      end
+
+      # Adds a validator to the current attribute.
+      #
+      # @param case_equality_or_proc [#===, Proc, nil] The validator to add
+      # @yield An optional block to use as the validator
+      # @return [self] the Builder instance
+      # @rbs (
+      #   Class | Module | ^(untyped value) -> bool case_equality_or_proc,
+      #   ) ?{ (?) -> void } -> self
+      def validate_with(case_equality_or_proc = nil, &block)
+        ensure_current_definition!
+        current_definition[:attribute_options][:validators] << (case_equality_or_proc || block)
+        self
+      end
+      alias validates validate_with
+
+      private
+
+      # Builds all pending attribute definitions.
+      #
+      # @return [void]
+      # @rbs () -> void
+      def build_all_definitions
+        @data.each_value { |definition_options| build_definition(definition_options) }
+      end
+
+      # Sets up a new attribute definition.
+      #
+      # @param attribute_name [String, Symbol] The name of the attribute
+      # @param type_validator [Class, Module, Proc] Optional type constraint
+      # @param options [Hash] Configuration options for the attribute
+      # @return [void]
+      # @rbs (
+      #   String | Symbol attribute_name,
+      #   (Class | Module | ^(untyped value) -> bool) type_validator,
+      #   **untyped options
+      #   ) -> void
+      def build_current_definition(attribute_name, type_validator, **options)
+        @current_definition = @data[attribute_name.to_sym] ||= {}
+        current_definition.merge!(reader: options.fetch(:reader, :public), writer: options.fetch(:writer, :public))
+        build_current_definition_attribute_options(
+          attribute_name,
+          type_validator,
+          **options.transform_keys(&:to_sym).except(:reader, :writer)
+        )
+      end
+
+      # Sets up the options for the current attribute definition.
+      #
+      # @param attribute_name [String, Symbol] The name of the attribute
+      # @param type_validator [Class, Module, Proc] Optional type constraint
+      # @param options [Hash] Configuration options for the attribute
+      # @return [void]
+      # @rbs (
+      #   String | Symbol attribute_name,
+      #   Class | Module | ^(untyped value) -> bool type_validator,
+      #   **untyped options
+      #   ) -> void
+      def build_current_definition_attribute_options(attribute_name, type_validator, **options)
+        attribute_options = current_definition[:attribute_options] ||=
+          Attribute::DEFAULT_OPTIONS.transform_values(&:dup)
+        attribute_options[:name] ||= attribute_name.to_sym
+        attribute_options.merge!(**options)
+        attribute_options[:validators] << type_validator if type_validator
+      end
+
+      # Constructs and injects an attribute from its definition.
+      #
+      # @param definition_options [Hash] The complete attribute definition
+      # @return [void]
+      # @rbs (Hash[Symbol, untyped] definition_options) -> void
+      def build_definition(definition_options)
+        attribute = Attribute.new(@base, **definition_options[:attribute_options])
+        definition = AttributeDefinition.new(attribute, **definition_options.except(:attribute_options))
+        @method_injector.inject!(definition)
+        @base.send(:__attribute_definitions__)[definition.attribute.name] = definition
+      end
+
+      # Retrieves the current attribute definition being built.
+      #
+      # @return [Hash] The current definition or an empty hash
+      # @rbs () -> Hash[Symbol, untyped]
+      def current_definition
+        @current_definition || {}
+      end
+
+      # Ensures there is a current attribute definition to modify.
+      #
+      # @raise [RuntimeError] If no attribute is currently being defined
+      # @return [void]
+      def ensure_current_definition!
+        raise 'No current definition to modify. Call #define first.' if @current_definition.nil?
+      end
+    end
+  end
+end
---
+++ domainic-attributer/spec/domainic/attributer/builder_spec.rb
@@ -0,0 +1,199 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Domainic::Attributer::Builder do
+  let(:base) { Class.new }
+
+  describe '#initialize' do
+    subject(:initialize_builder) { described_class.new(base) }
+
+    it 'is expected to create a new Builder' do
+      expect(initialize_builder).to be_a(described_class)
+    end
+  end
+
+  describe '#build!' do
+    subject(:build!) { builder.build! }
+
+    let(:builder) { described_class.new(base) }
+
+    context 'with no definitions' do
+      it 'is expected to return self' do
+        expect(build!).to eq(builder)
+      end
+    end
+
+    context 'with a pending definition' do
+      let(:definitions) { {} }
+
+      before do
+        allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+        builder.define(:test)
+      end
+
+      it 'is expected to add the definition to the base' do
+        build!
+        expect(definitions[:test]).to be_a(Domainic::Attributer::AttributeDefinition)
+      end
+
+      it 'is expected to clear the current definition' do
+        build!
+        expect { builder.required }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
+  describe '#coerce_with' do
+    subject(:coerce_with) { builder.coerce_with(coercer) }
+
+    let(:builder) { described_class.new(base) }
+    let(:coercer) { lambda(&:to_s) }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to add the coercer' do
+      coerce_with.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.send(:instance_variable_get, :@options)[:coercers]).to include(coercer)
+    end
+  end
+
+  describe '#define' do
+    subject(:define) do
+      builder.define(name, definition_params[:validator], **definition_params[:options], &definition_params[:block])
+    end
+
+    let(:builder) { described_class.new(base) }
+    let(:name) { :test }
+    let(:definition_params) { { validator: nil, options: {}, block: nil } }
+
+    it 'is expected to return self' do
+      expect(define).to eq(builder)
+    end
+
+    context 'with a type validator' do
+      let(:definition_params) { { validator: String, options: {} } }
+      let(:definitions) { {} }
+
+      before { allow(base).to receive(:__attribute_definitions__).and_return(definitions) }
+
+      it 'is expected to set the validator' do
+        define.build!
+        attribute = definitions[:test].attribute
+        expect(attribute.send(:instance_variable_get, :@options)[:validators]).to include(String)
+      end
+    end
+
+    context 'with a configuration block' do
+      let(:definition_params) { { validator: nil, options: {}, block: -> { required } } }
+      let(:definitions) { {} }
+
+      before { allow(base).to receive(:__attribute_definitions__).and_return(definitions) }
+
+      it 'is expected to execute the block' do
+        define.build!
+        attribute = definitions[:test].attribute
+        expect(attribute.required?).to be true
+      end
+    end
+  end
+
+  describe '#default' do
+    subject(:default) { builder.default(value) }
+
+    let(:builder) { described_class.new(base) }
+    let(:value) { 'test' }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to set the default value' do
+      default.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.default).to eq('test')
+    end
+  end
+
+  describe '#description' do
+    subject(:description) { builder.description(text) }
+
+    let(:builder) { described_class.new(base) }
+    let(:text) { 'test description' }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to set the description' do
+      description.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.description).to eq(text)
+    end
+  end
+
+  describe '#on_change' do
+    subject(:on_change) { builder.on_change(callback) }
+
+    let(:builder) { described_class.new(base) }
+    let(:callback) { ->(value) { value } }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to add the callback' do
+      on_change.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.send(:instance_variable_get, :@options)[:callbacks]).to include(callback)
+    end
+  end
+
+  describe '#required' do
+    subject(:required) { builder.required }
+
+    let(:builder) { described_class.new(base) }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to make the attribute required' do
+      required.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.required?).to be true
+    end
+  end
+
+  describe '#validate_with' do
+    subject(:validate_with) { builder.validate_with(validator) }
+
+    let(:builder) { described_class.new(base) }
+    let(:validator) { ->(value) { value.is_a?(String) } }
+    let(:definitions) { {} }
+
+    before do
+      allow(base).to receive(:__attribute_definitions__).and_return(definitions)
+      builder.define(:test)
+    end
+
+    it 'is expected to add the validator' do
+      validate_with.build!
+      attribute = definitions[:test].attribute
+      expect(attribute.send(:instance_variable_get, :@options)[:validators]).to include(validator)
+    end
+  end
+end
