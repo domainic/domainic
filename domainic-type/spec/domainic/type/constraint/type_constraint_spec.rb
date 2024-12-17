1cb479ced074dc9cdbf4fbaae0f2174d4afe7794
Aaron Allen <hello@aaronmallen.me>
2024-12-16 21:43:13 -0600

    Add `Domainic::Type::Constraint::TypeConstraint`
    Introduces the TypeConstraint class that provides core type validation
    functionality for the Domainic typing system. This constraint allows
    for validating values against Ruby classes, modules, and custom type
    behaviors.
    
    Key features:
      - Support for Class and Module type validation
      - Custom type validation through case equality (===)
      - Nil type handling through NilClass coercion
      - Type validation error messaging with proper articles
    
    The constraint serves as a foundational component for building more
    complex type validation rules in the Domainic system.
    
    Changelog:
      - added `Domainic::Type::Constraint::TypeConstraint`
    
    see #38

---
+++ domainic-type/spec/domainic/type/constraint/type_constraint_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/constraint/constraints/type_constraint'
+
+RSpec.describe Domainic::Type::Constraint::TypeConstraint do
+  shared_examples 'coerces and validates expectation' do
+    context 'when the expected type is valid' do
+      let(:expected_type) { String }
+
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context 'when the expected type is nil' do
+      let(:expected_type) { nil }
+
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context 'when the expected type is invalid', rbs: :skip do
+      let(:expected_type) { 'not a class, module, or type' }
+
+      it { expect { subject }.to raise_error(ArgumentError, /must be a Class, Module, or Domainic::Type/) }
+    end
+  end
+
+  describe '.new' do
+    subject(:constraint) { described_class.new(:self, expected_type) }
+
+    include_examples 'coerces and validates expectation'
+  end
+
+  describe '#description' do
+    subject(:description) { constraint.description }
+
+    let(:constraint) { described_class.new(:self, expected_type) }
+    let(:expected_type) { Array }
+
+    it { is_expected.to eq('Array') }
+  end
+
+  describe '#expecting' do
+    subject(:expecting) { constraint.expecting(expected_type) }
+
+    let(:constraint) { described_class.new(:self) }
+
+    include_examples 'coerces and validates expectation'
+  end
+
+  describe '#satisfied?' do
+    subject(:satisfied?) { constraint.satisfied?(actual_value) }
+
+    let(:constraint) { described_class.new(:self, expected_type) }
+
+    context 'when the actual value is an instance of the expected type' do
+      let(:expected_type) { Integer }
+      let(:actual_value) { 42 }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when the actual value is not an instance of the expected type' do
+      let(:expected_type) { Integer }
+      let(:actual_value) { 3.14 }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#violation_description' do
+    subject(:violation_description) { constraint.violation_description }
+
+    before do
+      constraint.satisfied?(actual_value)
+    end
+
+    let(:constraint) { described_class.new(:self, expected_type) }
+    let(:actual_value) { [] }
+    let(:expected_type) { Integer }
+
+    it { is_expected.to eq(actual_value.class.to_s) }
+  end
+end
