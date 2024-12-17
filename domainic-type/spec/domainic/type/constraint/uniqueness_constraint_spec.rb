a79b16cc4114b7a50c83db942cc22a8930555f27
Aaron Allen <hello@aaronmallen.me>
2024-12-17 01:00:15 -0600

    Add `Domainic::Type::Constraint::UniquenessConstraint`
    Introduces the UniquenessConstraint class that validates whether all
    elements in a collection are unique. This constraint operates on any
    enumerable object and verifies that no duplicate elements exist.
    
    Key features:
      - Works with any Enumerable collection
      - Simple unique element validation
      - Clear error messaging
      - No configuration required
      - Efficient validation using Ruby's built-in uniq method
    
    Changelog:
      - added `Domainic::Type::Constraint::UniquenessConstraint`
    
    see #40

---
+++ domainic-type/spec/domainic/type/constraint/uniqueness_constraint_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/constraint/constraints/uniqueness_constraint'
+require 'set'
+
+RSpec.describe Domainic::Type::Constraint::UniquenessConstraint do
+  describe '.new' do
+    subject(:constraint) { described_class.new(:self) }
+
+    it { expect { constraint }.not_to raise_error }
+  end
+
+  describe '#description' do
+    subject(:description) { constraint.description }
+
+    let(:constraint) { described_class.new(:self) }
+
+    it { is_expected.to eq('unique') }
+  end
+
+  describe '#satisfied?' do
+    subject(:satisfied?) { constraint.satisfied?(actual_value) }
+
+    let(:constraint) { described_class.new(:self) }
+
+    context 'with arrays' do
+      context 'when all elements are unique' do
+        let(:actual_value) { [1, 2, 3] }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when there are duplicate elements' do
+        let(:actual_value) { [1, 2, 2, 3] }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    context 'with sets' do
+      context 'when all elements are unique' do
+        let(:actual_value) { Set[1, 2, 3] }
+
+        it { is_expected.to be true }
+      end
+    end
+
+    context 'with strings' do
+      context 'when all characters are unique' do
+        let(:actual_value) { %w[a b c] }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when there are duplicate characters' do
+        let(:actual_value) { %w[a b b c] }
+
+        it { is_expected.to be false }
+      end
+    end
+  end
+
+  describe '#violation_description' do
+    subject(:violation_description) { constraint.violation_description }
+
+    let(:constraint) { described_class.new(:self) }
+
+    it { is_expected.to eq('not unique') }
+  end
+end
