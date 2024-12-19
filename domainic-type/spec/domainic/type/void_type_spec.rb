92f1f1ac8ca2d2a4314787053fadcfab0a9df741
Aaron Allen <hello@aaronmallen.me>
2024-12-18 23:10:41 -0600

    Add `Domainic::Type::VoidType`
    resolves #57

---
+++ domainic-type/spec/domainic/type/void_type_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domainic/type/types/void_type'
+
+RSpec.describe Domainic::Type::VoidType do
+  describe '#validate' do
+    subject(:validate) { type.validate(value) }
+
+    let(:type) { described_class.new }
+    let(:value) { [123, 1.2, 'abc', nil, true, false, Object.new, [], {}, :symbol].sample }
+
+    it { is_expected.to be true }
+  end
+end
