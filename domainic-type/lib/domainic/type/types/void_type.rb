9ebabd825b56785d3557a3c1ac6a4b4277b35f15
Aaron Allen <hello@aaronmallen.me>
2024-12-18 23:28:44 -0600

    Add `Domainic::Type::VoidType` (#80)
    resolves #57

---
+++ domainic-type/lib/domainic/type/types/void_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'domainic/type/behavior'
+
+module Domainic
+  module Type
+    # @since 0.1.0
+    class VoidType
+      include Behavior
+    end
+  end
+end
